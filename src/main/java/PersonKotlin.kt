d4d599cf52ddb03fffc3137166fb2a83e9c58992
user <sylvie.wu@hotmail.com>
2022-06-24 16:44:31 +0800

    Person kotlin

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,14 @@
+package com.sylvie
+
+
+class PersonKotlin {
+    fun greeting(){
+        println("hello world!")
+    }
+}
+
+//Top-level
+fun main(){
+    val p =PersonKotlin()
+    p.greeting()
+}
